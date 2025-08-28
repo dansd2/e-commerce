31f4fd08b4f5b78a4947ff6f6fcee2f59c0d9de8
danipxx <danizin1213@gmail.com>
2025-08-28 10:19:22 -0300

    cp/src/main.jsx

--- cp/src/main.jsx
+++ cp/src/main.jsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
-import App from './routes/App.jsx';
+import Cadastro from './pages/cadastro/cadastro.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Cadastro />
   </StrictMode>
 );
